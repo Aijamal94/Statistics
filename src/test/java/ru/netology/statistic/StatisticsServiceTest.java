799de989bcccf3941a4c29900456f1d41a82e309
Aizhamal <aijashka79@gmail.com>
2024-05-30 12:28:51 +0500

    Initial commit

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -1,2 +1,55 @@
-package PACKAGE_NAME;public class StatisticsServiceTest {
+package ru.netology.statistic;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class StatisticsServiceTest {
+
+    @Test
+    void findMax() {
+        ru.netology.statistic.StatisticsService service = new ru.netology.statistic.StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxWhenNotAtFirstPosition() {
+        ru.netology.statistic.StatisticsService service = new ru.netology.statistic.StatisticsService();
+
+        long[] incomesInBillions = {5, 12, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxWhenAllElementsAreEqual() {
+        ru.netology.statistic.StatisticsService service = new ru.netology.statistic.StatisticsService();
+
+        long[] incomesInBillions = {5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5};
+        long expected = 5;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxWhenNegativeNumbersArePresent() {
+        ru.netology.statistic.StatisticsService service = new ru.netology.statistic.StatisticsService();
+
+        long[] incomesInBillions = {5, -12, 8, 4, 5, 3, -8, 6, 11, 11, -12};
+        long expected = 11;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
